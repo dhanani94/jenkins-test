ac16f0e5bd4be3058e331114d1f1e6378371ef77
Taufiq Dhanani <dhanani94@gmail.com>
2017-12-12 09:41:05 -0500

    added for loop

--- Test.java
+++ Test.java
@@ -1,6 +1,9 @@
 public class Test {
 	public static void main(String[] args){
 		System.out.println("Hello friend");
-		System.out.println("taufiq is coolest bro ever!");
+		for (int i = 0;i<10 ;i++ ) {
+			System.out.println("taufiq is coolest bro ever!");
+			System.out.println("count = " + i);
+		}
 	}
 }
